4e52ad37308c6520556443f4647f5cb3998c2277
Saki-Ota <saki.ota01@outlook.com>
2022-09-05 19:39:34 +1000

    refactor: edited code to include text and img within a tag

--- DOM_creation/Lesson2/main.js
+++ DOM_creation/Lesson2/main.js
@@ -3,12 +3,13 @@ const li = document.createElement('li');
 
 const a = document.createElement('a');
 a.setAttribute("href", "1.html");
+const text = document.createTextNode("これです");
 
 const img = document.createElement('img');
 img.setAttribute("src", "bookmark.png");
 img.setAttribute("alt", "ブックマーク");
-img.textContent = "これです";
 
-li.appendChild(a);
 a.appendChild(img);
+a.appendChild(text);
+li.appendChild(a);
 ul.appendChild(li);
