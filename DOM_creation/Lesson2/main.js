2bd558e54c0a666cb6b03fbb868c3f9a2ef69ebe
Saki-Ota <saki.ota01@outlook.com>
2022-09-06 20:06:13 +1000

    fix: fixed code to display text within a tag

--- DOM_creation/Lesson2/main.js
+++ DOM_creation/Lesson2/main.js
@@ -3,11 +3,10 @@ const li = document.createElement('li');
 
 const a = document.createElement('a');
 a.href = "1.html";
-
-const text = document.createTextNode("これです");
+a.textContent = 'これです';
 
 const img = document.createElement('img');
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-ul.appendChild(li).appendChild(a).appendChild(img).appendChild(text);
+ul.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img)
