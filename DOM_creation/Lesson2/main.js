8c5a8c26c94e70df408e70620140caee8151c8ab
Saki-Ota <saki.ota01@outlook.com>
2022-09-06 19:23:08 +1000

    refactor: edited some codes based on review comments

--- DOM_creation/Lesson2/main.js
+++ DOM_creation/Lesson2/main.js
@@ -2,14 +2,12 @@ const ul= document.getElementById("js-ul");
 const li = document.createElement('li');
 
 const a = document.createElement('a');
-a.setAttribute("href", "1.html");
+a.href = "1.html";
+
 const text = document.createTextNode("これです");
 
 const img = document.createElement('img');
-img.setAttribute("src", "bookmark.png");
-img.setAttribute("alt", "ブックマーク");
+img.src = "bookmark.png";
+img.alt = "ブックマーク";
 
-a.appendChild(img);
-a.appendChild(text);
-li.appendChild(a);
-ul.appendChild(li);
+ul.appendChild(li).appendChild(a).appendChild(img).appendChild(text);
