b3dd714c16c38e5e03791e01c5837dd498a442b9
Saki-Ota <saki.ota01@outlook.com>
2022-09-21 20:24:39 +1000

    refactor: chaged name of the array and reorder the codes inside of for of method

--- DOM_creation/Lesson4/main.js
+++ DOM_creation/Lesson4/main.js
@@ -1,16 +1,16 @@
 const ul = document.getElementById("js-ul");
 const fragment = new DocumentFragment();
-const itemsToInsert = [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}];
+const features = [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+{to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}];
 
-for (let item of itemsToInsert ){
+for (let feature of features ){
   const li = document.createElement('li');
   const a = document.createElement('a');
-  a.href = item.to;
-  a.textContent = item.text;
-
   const img = document.createElement('img');
-  img.src = item.img;
-  img.alt = item.alt
+  a.href = feature.to;
+  a.textContent = feature.text;
+  img.src = feature.img;
+  img.alt = feature.alt
 
   fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
 }
