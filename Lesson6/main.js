7bbd88caac06bdd5702349563d2bb54b88ddaf2c
Saki-Ota <saki.ota01@outlook.com>
2022-09-26 21:47:35 +1000

    feat: promise resolve process is triggered after 3 seconds

---
+++ Lesson6/main.js
@@ -0,0 +1,25 @@
+const ul = document.getElementById("js-ul");
+const fragment = new DocumentFragment();
+const features = [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+{to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}];
+
+const checkFeatures = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(features);
+  }, 3000);
+}).then((value)　=> {
+    for (let feature of value ){
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const img = document.createElement('img');
+      a.href = feature.to;
+      a.textContent = feature.text;
+      img.src = feature.img;
+      img.alt = feature.alt
+
+      fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+    }
+
+    ul.appendChild(fragment);
+  }
+);
