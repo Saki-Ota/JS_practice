183faeaf363c02096a4749af382e02758a5fe22f
Saki-Ota <saki.ota01@outlook.com>
2022-10-03 09:24:14 +1100

    refactor: changed promise name

--- Lesson8/main.js
+++ Lesson8/main.js
@@ -2,21 +2,21 @@ const addLoading = () =>{
   const loading = document.getElementById("js-loading");
   const loadingImg = document.createElement('img');
   loadingImg.src = 'loading.gif';
-  loadingImg.id = "loading-img"
-  loading.appendChild(loadingImg)
+  loadingImg.id = "loading-img";
+  loading.appendChild(loadingImg);
 }
 
-const promiseChecker = new Promise((resolve, reject) => {
+const promiseFeatures = new Promise((resolve, reject) => {
   addLoading();
   setTimeout(() => {
     reject("The promise was rejected!");
   }, 3000);
 });
 
-promiseChecker.then(
+promiseFeatures.then(
   (value) => {
-    console.log('OK', value)
+    console.log('OK', value);
 })
 .catch((error) =>{
-  console.error(error)
+  console.error(error);
 });
