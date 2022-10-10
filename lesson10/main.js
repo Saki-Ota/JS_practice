87d122e6c42efd3750b498efc59505ab21b0506c
Saki-Ota <saki.ota01@outlook.com>
2022-10-11 10:51:47 +1100

    feat: using try-catch-finally method to perform the same process as lesson9

---
+++ lesson10/main.js
@@ -0,0 +1,53 @@
+const ul = document.getElementById("js-ul");
+const loading = document.getElementById("js-loading")
+
+const addLoading = () =>{
+  const loadingImg = document.createElement('img');
+  loadingImg.src = 'loading.gif';
+  loadingImg.id = "loading-img";
+  loading.appendChild(loadingImg);
+};
+
+const removeLoading = () =>{
+  document.getElementById('loading-img').remove();
+};
+
+const createLists = (features) =>{
+  const fragment = new DocumentFragment();
+
+  for (let feature of features ){
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const img = document.createElement('img');
+      a.href = feature.to;
+      a.textContent = feature.text;
+      img.src = feature.img;
+      img.alt = feature.alt;
+
+      fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+    };
+
+    ul.appendChild(fragment);
+};
+
+
+const receiveData = new Promise((resolve) =>{
+  addLoading();
+  const features = [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+                    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}];
+  setTimeout(() => resolve(features), 3000)
+});
+
+const asyncFeatures = async () => {
+  try {
+    const result = await receiveData;
+    removeLoading();
+    createLists(result);
+  } catch(error) {
+    console.error(error);
+  } finally {
+    console.log('The process is completed!');
+  }
+};
+
+asyncFeatures();
